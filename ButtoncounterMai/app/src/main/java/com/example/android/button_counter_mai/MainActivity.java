e1eb01b74f0af696f32ebfe1af2d0612d94a48ba
Jasmine Mai <maijd@g.cofc.edu>
2018-01-27 13:16:56 -0500

    Added images and sounds

--- ButtoncounterMai/app/src/main/java/com/example/android/button_counter_mai/MainActivity.java
+++ ButtoncounterMai/app/src/main/java/com/example/android/button_counter_mai/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.button_counter_mai;
 
 import android.content.res.Resources;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,46 +10,64 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity{
     int quantity = 0;
+    TextView textView;
+    String amountClicks;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Resources res = getResources();
-        String amountClicks = String.format(res.getString(R.string.counter), quantity);
-        TextView textView = findViewById(R.id.textView);
-        textView.setText(amountClicks);
-        Button clearButton = findViewById(R.id.button);
-        Button incrementButton = findViewById(R.id.button2);
-        Button decrementButton = findViewById(R.id.button3);
-        clearButton.setOnClickListener(onClick);
-        incrementButton.setOnClickListener(onClick);
-        decrementButton.setOnClickListener(onClick);
-    }
-    private View.OnClickListener onClick = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            Resources res = getResources();
-            switch (view.getId()) {
-                case R.id.button:
-                    quantity = 0;
-                    String amountClicks = String.format(res.getString(R.string.counter), quantity);
-                    TextView textView = findViewById(R.id.textView);
-                    textView.setText(amountClicks);
-                    break;
-                case R.id.button2:
-                    quantity = quantity + 1;
-                    amountClicks = String.format(res.getString(R.string.counter), quantity);
-                    textView = findViewById(R.id.textView);
-                    textView.setText(amountClicks);
-                    break;
-                case R.id.button3:
-                    quantity = quantity - 1;
-                    amountClicks = String.format(res.getString(R.string.counter), quantity);
-                    textView = findViewById(R.id.textView);
-                    textView.setText(amountClicks);
-                    break;
-            }
-
+        final Resources res = getResources();
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.meow);
+        if (savedInstanceState != null)
+        {
+            quantity = savedInstanceState.getInt("quantity");
         }
-    };
+
+        Button incrementButton = findViewById(R.id.plus);
+        incrementButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mp.start();
+                quantity = quantity + 1;
+                amountClicks = String.format(res.getString(R.string.counter), quantity);
+                textView = findViewById(R.id.textView);
+                textView.setText(amountClicks);
+            }
+        });
+
+        Button decrementButton = findViewById(R.id.subtract);
+        decrementButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mp.start();
+                quantity = quantity - 1;
+                amountClicks = String.format(res.getString(R.string.counter), quantity);
+                textView = findViewById(R.id.textView);
+                textView.setText(amountClicks);
+            }
+        });
+
+        Button clearButton = findViewById(R.id.clear);
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mp.start();
+                quantity = 0;
+                amountClicks = String.format(res.getString(R.string.counter), quantity);
+                textView = findViewById(R.id.textView);
+                textView.setText(amountClicks);
+            }
+        });
+        textView = findViewById(R.id.textView);
+        amountClicks = String.format(res.getString(R.string.counter), quantity);
+        textView.setText(amountClicks);
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState)
+    {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt("quantity", quantity);
+    }
 }
